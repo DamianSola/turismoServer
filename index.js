d5e828b4683b629608fc17744ea85502da108e57
damian sola <damiansola99@gmail.com>
2023-05-19 22:12:48 -0300

    fff

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const cors = require('cors')
 
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.use(cors())
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+const port = process.env.PORT || 3001
+
+
+// Syncing all the models at once.
+conn.sync({ force: true }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log("%s listening at 3000"); // eslint-disable-line no-console
   });
 });
