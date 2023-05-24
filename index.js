a18c7c59715725600c5b1eb454c5bb0a8f27c8e3
damian sola <damiansola99@gmail.com>
2023-05-23 21:01:16 -0300

    agregue description a subcategory

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3001
 
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(port, () => {
     console.log("%s listening at 3000"); // eslint-disable-line no-console
   });
