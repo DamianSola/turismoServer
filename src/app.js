51da1cce4403cec07eded0397181ee0ffb1d6dea
damian sola <damiansola99@gmail.com>
2023-06-09 18:45:14 -0300

    cors-resolve

--- src/app.js
+++ src/app.js
@@ -12,12 +12,12 @@ const routes = require('./Routes/Index.js');
 const server = express();
 
 server.name = 'API';
-
+const originUrl = ['https://app-salta-turismo.vercel.app/',"*"]
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use(cors())
+server.use(cors({origin: originUrl}))
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://app-salta-turismo.vercel.app/'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
